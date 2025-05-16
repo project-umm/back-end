8ee26098bff0019825b7c21da73171dfb8427267
DongGuk-Seo <failnxid@gmail.com>
2025-05-16 17:07:33 +0900

    add : exception

--- src/main/java/com/umm/config/SecurityConfig.java
+++ src/main/java/com/umm/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.umm.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,25 +22,24 @@ import java.util.Arrays;
 public class SecurityConfig{
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfig corsConfig) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception -> exception.authenticationEntryPoint((request, response, unauthorizedException) -> {
+                                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                                    response.setContentType("application/json;charset=UTF-8");
+                                    response.getWriter().write("message: 인증되지 않은 접근입니다. \n error:" + unauthorizedException);})
+                                .authenticationEntryPoint((request, response, forbiddenException) -> {
+                                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                                    response.setContentType("application/json;charset=UTF-8");
+                                    response.getWriter().write("message: 허가되지 않은 접근입니다. \n error:" + forbiddenException);})
+                )
                 .authorizeHttpRequests(
                         auth -> auth.anyRequest().permitAll()
                 )
-                .addFilter(corsFilter())
+                .addFilter(corsConfig.corsFilter())
                 .build();
-    }
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**",config);
-        return new CorsFilter(source);
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
