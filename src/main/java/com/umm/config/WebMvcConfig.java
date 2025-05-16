4afa843957ba5f4e40b3ff7e7176757904b97a37
DongGuk-Seo <failnxid@gmail.com>
2025-05-16 13:29:07 +0900

    add: mvc config

---
+++ src/main/java/com/umm/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.umm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("server.project-umm.com","localhost:8000")
+                .allowedMethods("*");
+    }
+}
