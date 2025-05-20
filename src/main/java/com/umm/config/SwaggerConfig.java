33a64ef3d7edafdd0463f12f87c3bbc989fecdbc
DongGuk-Seo <failnxid@gmail.com>
2025-05-20 10:47:29 +0900

    fix: swagger urls

--- src/main/java/com/umm/config/SwaggerConfig.java
+++ src/main/java/com/umm/config/SwaggerConfig.java
@@ -3,17 +3,26 @@ package com.umm.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server().url("http://localhost:8000"));
+        servers.add(new Server().url("https://server.project-umm.com"));
+
         return new OpenAPI()
                 .components(new Components())
+                .servers(servers)
                 .info(info());
     }
 
