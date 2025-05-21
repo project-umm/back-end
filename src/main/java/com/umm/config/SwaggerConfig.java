7b1a1ac97dd85cd97ab5b6cadc3e38b03e3acac3
DongGuk-Seo <failnxid@gmail.com>
2025-05-21 17:17:15 +0900

    feat: swagger auth

--- src/main/java/com/umm/config/SwaggerConfig.java
+++ src/main/java/com/umm/config/SwaggerConfig.java
@@ -3,16 +3,21 @@ package com.umm.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig{
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -20,9 +25,22 @@ public class SwaggerConfig {
         servers.add(new Server().url("http://localhost:8000"));
         servers.add(new Server().url("https://server.project-umm.com"));
 
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("JWT");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("JWT", bearerAuth)
+                        )
                 .servers(servers)
+                .addSecurityItem(securityRequirement)
                 .info(info());
     }
 
@@ -33,10 +51,12 @@ public class SwaggerConfig {
                 .pathsToMatch("/api/**")
                 .build();
     }
-    private Info info() {
+    Info info() {
         return new Info()
                 .title("Umm API")
                 .description("Umm API")
                 .version("1.0");
     }
+
+
 }
