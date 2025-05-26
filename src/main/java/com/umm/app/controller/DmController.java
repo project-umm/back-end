f8ded1b27c9e78010b4000b51f2f8cc74dc0872a
DongGuk-Seo <failnxid@gmail.com>
2025-05-26 22:10:29 +0900

    fix: param snake

--- src/main/java/com/umm/app/controller/DmController.java
+++ src/main/java/com/umm/app/controller/DmController.java
@@ -40,7 +40,7 @@ public class DmController {
     }
 
     @GetMapping("/{dmId}")
-    public ResponseEntity<PageableDmChatResponse> listChats(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable UUID dmId, @RequestParam BigInteger key, @RequestParam Integer pageNumber) {
+    public ResponseEntity<PageableDmChatResponse> listChats(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable UUID dmId, @RequestParam BigInteger key, @RequestParam("page_number") Integer pageNumber) {
         PageableDmChatResponse response  = dmChatService.listDmChats(customUserDetails, dmId, key, pageNumber);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
